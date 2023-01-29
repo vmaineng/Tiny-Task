dac8e2bf8ffa5035fa6aa6ddf4015547679ca5ed
Mai Vang <vmaineng@gmail.com>
2023-01-29 14:19:25 -0800

    updated

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,14 @@
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
-bundle exec rails db:migrate
+# clean
+rm -rf public
+# build
+npm install --prefix client && npm run build --prefix client
+# migrate
+bundle exec rake db:migrate
+# postbuild
+cp -a client/build/. public/
