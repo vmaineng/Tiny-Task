cf884ab6981433f4b9dff40e78402b378a9a91af
Mai Vang <vmaineng@gmail.com>
2023-02-06 18:43:09 -0800

    updated script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,12 +3,10 @@
 # exit on error
 set -o errexit
 
-bundle install
-# clean
 rm -rf public
-# build
 npm install --prefix client && npm run build --prefix client
-# migrate
-bundle exec rake db:migrate
-# postbuild
 cp -a client/build/. public/
+
+bundle install
+bundle exec rake db:migrate 
+bundle exec rake db:seed
